c514e16b5bbc7d73628bf4f541009c0cb15ee4b6
hoang.bn <hoang.bn@teko.vn>
2024-04-29 13:20:56 +0700

    Init linkedin_scraper

---
+++ linkedin_scraper/linkedin_scraper/pipelines.py
@@ -0,0 +1,47 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+import os 
+from pymongo import MongoClient
+from .filters import *
+
+class LinkedinScraperPipeline:
+    def __init__(self):
+        client = MongoClient(os.getenv('MONGODB_URI'))
+        self.db = client["Grab-Data"]
+        self.job_collection = self.db["jobs"]
+    def process_item(self, item, spider):
+        if self.job_collection.find_one({'job_url': item['job_url'], 'company': item['company'], 'title': item['title'], 'date': item['date']}) is not None:
+            return item
+        
+        industries = []
+        if 'industry' in item['query']:
+            for code in item['query']['industry']:
+                industry_name = IndustryFilters(code).name
+                if industry_name:
+                    industries.append(industry_name)
+
+        
+        self.job_collection.insert_one(
+            {
+                'title': item['title'],
+                'company': item['company'],
+                'location': item['location'],
+                'date': item['date'],
+                'job_url': item['job_url'],
+                'job_description': item['job_description'],
+                'company_link': item['company_link'],
+                'company_location': item['company_location'],
+                'industry': industries,
+                'type': item['query']['type'] if 'type' in item['query'] else None,
+                'experience': item['query']['experience'] if 'experience' in item['query'] else None,
+                'keywords': item['query']['keywords'] if 'keywords' in item['query'] else None,
+                'time': item['query']['time'] if 'time' in item['query'] else None,
+            }
+        )
+        return item
---
+++ linkedin_scraper/main.py
@@ -0,0 +1,11 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from apscheduler.schedulers.twisted import TwistedScheduler
+
+from linkedin_scraper.spiders.linkedin_spyder import LinkedInScraperSpider # your spider
+
+process = CrawlerProcess(get_project_settings())
+scheduler = TwistedScheduler()
+scheduler.add_job(process.crawl, 'interval', args=[LinkedInScraperSpider], seconds=5)
+scheduler.start()
+process.start(False)
